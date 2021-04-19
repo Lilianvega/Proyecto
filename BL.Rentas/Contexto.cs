3eed5372b390446d54ca4ff686cfe0c05be51f00
Lilianvega <78711744+Lilianvega@users.noreply.github.com>
2021-04-18 20:47:52 -0600

    Add files via upload

---
+++ BL.Rentas/Contexto.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+
+
+namespace BL.Rentas
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(): base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\RentaVehiculo.mdf")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new DatosdeInicio());
+
+        }
+
+        public DbSet<Vehiculo> Vehiculo { get; set; }
+        public BindingList<Tipo> Tipo { get; internal set; }
+        public DbSet<Tipo> Tipos { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ BL.Rentas/Datos_Inicio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    public class DatosdeInicio : CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+
+            var tipo1 = new Tipo();
+            tipo1.Descripcion = "Turismo";
+            contexto.Tipos.Add(tipo1);
+
+            var tipo2 = new Tipo();
+            tipo2.Descripcion = "Pick up 2x4";
+            contexto.Tipos.Add(tipo2);
+
+            var tipo3 = new Tipo();
+            tipo3.Descripcion = "Pick up 4x4";
+            contexto.Tipos.Add(tipo3);
+
+
+            base.Seed(contexto);
+        }
+    }
+}
