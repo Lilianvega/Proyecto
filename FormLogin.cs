4572cb0c1d772c24a9cf69c354e9b49dfa9ed578
Lilianvega <78711744+Lilianvega@users.noreply.github.com>
2021-04-18 20:57:14 -0600

    Add files via upload

---
+++ FormLogin.cs
@@ -0,0 +1,111 @@
+﻿
+using BL.Rentas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Renta_de_vehiculos
+{
+    public partial class FormLogin : Form
+    {
+        SeguridadBL _seguridad;
+
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            _seguridad = new SeguridadBL();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+
+            var resultado = _seguridad.Autorizar(usuario, contrasena);
+
+            if (resultado == true)
+            {
+                Utils.nombreUsuario = "Administrador";
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contraseña Incorrecta");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)
+                && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox2.Focus();
+            }
+        }
+
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)
+                && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
